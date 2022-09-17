690174c0f410be3aa073a7c6efb0c20b33d8f239
Savage912 <55938321+Savage912-Official@users.noreply.github.com>
2022-09-17 11:07:37 -0400

    Initial Commit

---
+++ resources/88-bTricks/client/config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config.minimumSpeed = 10 -- km/h
+Config.groundHeight = 2.5 -- minimum height to perform and autocancel the superman animation.
+
+Config.allowedVehicles = {
+    [`crf450`] = true,
+    [`manchez`] = true,
+    [`sanchez`] = true,
+    [`diablous`] = true,
+    [`seashark`] = true,
+    [`fmx`] = true,
+}
+
+Config.tricks = {
+    [1] = {label = 'Bike Trick: Surfing', duration = 2700, dict = 'rcmextreme2atv', anim = 'idle_e', flag = 35, flag2 = 4127},
+    [2] = {label = 'Bike Trick: Nac-Nac left', duration = 4500, dict = 'rcmextreme2atv', anim = 'idle_b', flag = 35, flag2 = 4127},
+    [3] = {label = 'Bike Trick: Nac-Nac right', duration = 3500, dict = 'rcmextreme2atv', anim = 'idle_c', flag = 35, flag2 = 4127},
+    [4] = {label = 'Bike Trick: Lazy Boy', duration = 4790, dict = 'rcmextreme2atv', anim = 'idle_a', flag = 35, flag2 = 4127},
+    [5] = {label = 'Bike Trick: Side to side', duration = 5300, dict = 'rcmextreme2atv', anim = 'idle_d', flag = 35, flag2 = 4127},
+    [6] = {label = 'Bike Trick: Air Shagging', duration = 3500, dict = 'anim@mp_player_intcelebrationmale@air_shagging', anim = 'air_shagging', flag = 48, flag2 = 4127},
+    [7] = {label = 'Bike Trick: Superman', duration = 0, dict = 'skydive@freefall', anim = 'free_back', flag = 35, flag2 = 4127},
+    [8] = {label = 'Bike Trick: Celebrate', duration = 4500, dict = 'mini@racing@bike@', anim = 'celebrate_c', flag = 35, flag2 = 4127}
+}
